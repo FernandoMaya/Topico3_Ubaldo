a6a997ac55b9c067f84977ce354d2d4768061b8b
Fernando Maya <34661669+FernandoMaya@users.noreply.github.com>
2021-12-23 16:19:00 -0600

    Add files via upload

---
+++ Éxito/funciones.js
@@ -0,0 +1,15 @@
+function results()
+{
+	var rest = 0;
+	for (var i = 0; i < 5; i++) {
+		rest = rest + parseInt(document.querySelector('input[name="q' + (i+1) + '"]:checked').value);
+		//rest.push(document.querySelector('input[name="q' + i + '"]:checked').value);
+	}
+	console.log(rest);
+ 
+	info = document.getElementById('test');
+	tit = document.getElementById('titCuest');
+	info.innerHTML	= "<h3 id='restCuest'>Has acertado en " + rest + "/5 preguntas</h3>";
+	tit.innerHTML	= "Resultados";
+	window.location = "#activity"
+}
